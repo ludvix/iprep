7c5f3154c56e95501a451c9b831620b965e40b00
Ludvig <kalbergludvig@outlook.com>
2026-04-04 20:10:38 +0200

    add http request, ssl and headers

--- main.rb
+++ main.rb
@@ -1,7 +1,11 @@
 require 'net/http'
 ip = ARGV[0]
-api_key = ENV["ABUSEIDPD_KEY"]
+api_key = ENV["ABUSEIPBD_KEY"]
 uri = URI("https://api.abuseipdb.com/api/v2/check")
 request = Net::HTTP::Get.new(uri)
 request["Key"] = api_key
 request["Accept"] = "application/json"
+Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
+  response = http.request(request)
+  puts response.body
+end
