f042e7aeceee8ba81fb71dfdf3f48f646dd661c5
Ludvig <kalbergludvig@outlook.com>
2026-04-05 11:47:32 +0200

    add error handling for ip-address and api key

--- main.rb
+++ main.rb
@@ -1,7 +1,15 @@
 require 'net/http'
 require 'json'
+if ARGV.empty?
+  puts "Command must be: ruby main.rb ip-address"
+  exit
+end
 ip = ARGV[0]
 api_key = ENV["ABUSEIPDB_KEY"]
+if api_key.nil?
+  puts "API key is not set. Command must be: export ABUSEIPDB_KEY=yourkey"
+  exit
+end
 uri = URI("https://api.abuseipdb.com/api/v2/check")
 uri.query = URI.encode_www_form({"ipAddress" => ip , "maxAgeInDays" => 90})
 request = Net::HTTP::Get.new(uri)
